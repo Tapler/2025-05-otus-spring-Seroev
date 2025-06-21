64610daeb6fbfd50dcae6c61d006a2e219604d30
vseroev <seroevvlad@mail.ru>
2025-06-21 22:41:58 +0300

    task03 - ошибка в тесте

--- homework01/src/test/java/ru/otus/vseroev/service/QuestionServiceIntegrationTest.java
+++ homework01/src/test/java/ru/otus/vseroev/service/QuestionServiceIntegrationTest.java
@@ -3,9 +3,12 @@ package ru.otus.vseroev.service;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.otus.vseroev.config.AppProperties;
 import ru.otus.vseroev.dao.QuestionDaoCsv;
 
@@ -15,7 +18,8 @@ import java.io.PrintStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = ru.otus.vseroev.config.AppConfig.class)
 @TestPropertySource("classpath:application-test.properties")
 class QuestionServiceIntegrationTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
